bc70a2e53d8a168a73df8c171e89c78f23ee601a
João Santiago <exalowprofile@gmail.com>
2021-04-16 22:54:22 +0200

    add like a lot more 😱

--- data-raw/free_email_providers.R
+++ data-raw/free_email_providers.R
@@ -3,13 +3,18 @@
 #   "https://gist.githubusercontent.com/tbrianjones/5992856/raw/93213efb652749e226e69884d6c048e595c1280a/free_email_provider_domains.txt"
 #   )
 
+arnt_freemail <- readLines("https://raw.githubusercontent.com/arnt/freemail/master/freemail.txt")
+
 free_email_providers_domains <- c(
-  "gmail.com", "zohomail.eu", "zohomail.de", "zohomail.com", "zoho.com", "zoho.de", "gmx.de", "gmx.biz", "gmx.org", "gmx.net", "firemail.eu", "firemail.de", "ok.de", "vip-client.de", "outlook.de", "outlook.com", "web.de", "mail.de", "yahoo.de", "yahoo.com", "aol.com", "aol.de", "icloud.com", "freenet.de",
+  "gmail.com", "zohomail.eu", "zohomail.de", "zohomail.com", "zoho.com", "zoho.de", "gmx.de", "gmx.biz", "gmx.org", "gmx.net", "firemail.eu", "firemail.de", "firemail.at", "ok.de", "vip-client.de", "outlook.de", "outlook.com", "web.de", "mail.de", "yahoo.de", "yahoo.com", "aol.com", "aol.de", "icloud.com", "freenet.de",
   "mailbox.org", "posteo.de", "t-online.de", "hotmail.com", "hotmail.de", "aim.com", "googlemail.com",
   "tuta.io", "protonmail.com", "online.de", "email.de", "mein.gmx",
-  "wp.pl", "yopmail.com", "vodafonemail.de", "tutanota.com", "skymail.de",
-  "bizz.de", "gmx-topmail.de", "maills.de", "magenta.de", "bossmail.de", "emailn.de", "bk.ru", "yandex.ru", "hey.com", "live.com", "yandex.mail", "mail.ru", "list.ru", "inbox.ru"
+  "wp.pl", "yopmail.com", "vodafonemail.de", "tutanota.com", "skymail.de", "skymail.at",
+  "bizz.de", "gmx-topmail.de", "maills.de", "magenta.de", "bossmail.de", "emailn.de", "bk.ru", "yandex.ru", "hey.com", "live.com", "yandex.mail", "mail.ru", "list.ru", "inbox.ru", "freemail.biz", "freemail.de", "xemail.at", "xemail.be", "xemail.cc", "xemail.ch", "xemail.de", "xemail.li",
+  "xemail.me"
 )
+
+free_providers <- unique(c(arnt_freemail, free_email_providers_domains))
 
 free_emails <- data.frame(
   type = "free non-burner",
@@ -17,7 +22,7 @@ free_emails <- data.frame(
 )
 
 writeLines(
-  unique(free_email_providers_domains[order(free_email_providers_domains)]),
+  unique(free_providers[order(free_providers)]),
   here::here("data-raw/free_email_providers_domains.txt")
 )
 
