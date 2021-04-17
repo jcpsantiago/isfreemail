e714df4155b5f1f1ee7bdd9f2bef95689df6cf45
João Santiago <exalowprofile@gmail.com>
2021-04-17 09:09:57 +0200

    a bunch more from 5x2.de and its funky aliases

--- data-raw/free_email_providers.R
+++ data-raw/free_email_providers.R
@@ -5,13 +5,8 @@
 
 arnt_freemail <- readLines("https://raw.githubusercontent.com/arnt/freemail/master/freemail.txt")
 
-free_email_providers_domains <- c(
-  "gmail.com", "zohomail.eu", "zohomail.de", "zohomail.com", "zoho.com", "zoho.de", "gmx.de", "gmx.biz", "gmx.org", "gmx.net", "firemail.eu", "firemail.de", "firemail.at", "ok.de", "vip-client.de", "outlook.de", "outlook.com", "web.de", "mail.de", "yahoo.de", "yahoo.com", "aol.com", "aol.de", "icloud.com", "freenet.de",
-  "mailbox.org", "posteo.de", "t-online.de", "hotmail.com", "hotmail.de", "aim.com", "googlemail.com",
-  "tuta.io", "protonmail.com", "online.de", "email.de", "mein.gmx",
-  "wp.pl", "yopmail.com", "vodafonemail.de", "tutanota.com", "skymail.de", "skymail.at",
-  "bizz.de", "gmx-topmail.de", "maills.de", "magenta.de", "bossmail.de", "emailn.de", "bk.ru", "yandex.ru", "hey.com", "live.com", "yandex.mail", "mail.ru", "list.ru", "inbox.ru", "freemail.biz", "freemail.de", "xemail.at", "xemail.be", "xemail.cc", "xemail.ch", "xemail.de", "xemail.li",
-  "xemail.me"
+free_email_providers_domains <- readLines(
+  here::here("data-raw/free_email_providers_domains.txt")
 )
 
 free_providers <- unique(c(arnt_freemail, free_email_providers_domains))
